0eae63b0243db5fcb6e5f0d53fcbf0a5934e67cb
Vinod Krishnan <vin.sinin@gmail.com>
2017-10-21 21:33:58 -0700

    changes

---
+++ src/javarestapi/rest/StringService.java
@@ -0,0 +1,17 @@
+package javarestapi.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+@Path("/rest")
+@Produces(MediaType.APPLICATION_JSON)
+public class StringService {
+    
+    @GET
+    public String get(){
+        return "Resume";
+    }
+}
