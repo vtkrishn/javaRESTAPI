b221ca1208c4d038b0b8b882ddf5f783313531c4
Vinod Krishnan <vin.sinin@gmail.com>
2017-10-22 00:32:22 -0700

    maven

--- src/com/resumely/rest/ResumeService.java
+++ src/com/resumely/rest/ResumeService.java
@@ -19,34 +19,35 @@ import javax.ws.rs.core.MediaType;
 public class ResumeService {
     
     @GET
-    public String getResume(){
+    public String get(){
         
         
         //create the file with the command argument
-        AbstractFile resume = new Resume("com\\resumely\\resume.doc");
-        //read the content row by row from the document
-        ResumeUtil.getInstance().readContent(resume);  
-        //extract word by word
-        LineList list = new LineList();
-        Parser.lineTokenize(resume.getContent(),list);
-        //identify first 10 lines and work on those words
-//        for(int i=0;i<list.getLines().size();i++){
-//            //System.out.println(list.getLines().get(i).getWordCount());
-//            Line line = list.getLines().get(i);
+//        AbstractFile resume = new Resume("com\\resumely\\resume.doc");
+//        //read the content row by row from the document
+//        ResumeUtil.getInstance().readContent(resume);  
+//        //extract word by word
+//        LineList list = new LineList();
+//        Parser.lineTokenize(resume.getContent(),list);
+//        //identify first 10 lines and work on those words
+////        for(int i=0;i<list.getLines().size();i++){
+////            //System.out.println(list.getLines().get(i).getWordCount());
+////            Line line = list.getLines().get(i);
+////        }
+//        
+//        StringBuilder result = new StringBuilder();
+//        result.append("{ \"result\" : [");
+//        for(Map.Entry<String,Integer> entry : list.getWordMap().entrySet()){
+//                    //System.out.println(entry.getKey() + ":" + entry.getValue());
+//                    result.append("{ \""+ entry.getKey() +"\" : ");
+//                    result.append("{ \""+ entry.getValue() +"\" }");
+//                    result.append(",");
 //        }
-        
-        StringBuilder result = new StringBuilder();
-        result.append("{ \"result\" : [");
-        for(Map.Entry<String,Integer> entry : list.getWordMap().entrySet()){
-                    //System.out.println(entry.getKey() + ":" + entry.getValue());
-                    result.append("{ \""+ entry.getKey() +"\" : ");
-                    result.append("{ \""+ entry.getValue() +"\" }");
-                    result.append(",");
-        }
-        int length = result.toString().length();
-        result.deleteCharAt(length-1);
-        result.append("]");
-        result.append("}");
-        return result.toString();
+//        int length = result.toString().length();
+//        result.deleteCharAt(length-1);
+//        result.append("]");
+//        result.append("}");
+//        return result.toString();
+        return "{}";
     }
 }
