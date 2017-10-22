e42a8bf10f6d9b1393184506fe8b25da2fec8b2d
Vinod Krishnan <vin.sinin@gmail.com>
2017-10-21 22:14:08 -0700

    changes

---
+++ src/javarestapi/server/ServerMain.java
@@ -0,0 +1,28 @@
+package javarestapi.server;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+public class ServerMain {
+    public ServerMain() {
+        super();
+    }
+    
+    public static void main(String ... args){
+        String webappDirLocation = "src/webapp/";
+        String webPort = System.getenv("PORT");
+                if (webPort == null || webPort.isEmpty()) {
+                    webPort = "80";
+        }
+        Server server = new Server(Integer.valueOf(webPort)); 
+        WebAppContext root = new WebAppContext();
+        root.setContextPath("/");
+                root.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
+        root.setResourceBase(webappDirLocation);
+        root.setParentLoaderPriority(true);
+
+                server.setHandler(root);
+
+                server.start();
+        server.join();
+    }
+}
